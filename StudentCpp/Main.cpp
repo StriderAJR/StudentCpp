2236823b9a350b69ffc6ff6104d43e1a07b0a6fd
Alex J <alexander.aj.ranger@gmail.com>
2017-01-02 01:29:04 +0500

    Удалены лишние файлы

--- StudentCpp/Main.cpp
+++ StudentCpp/Main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "StructArray.h"
 #include "ConsoleDraw.h"
+#include "AssocArray.h"
 
 using namespace std;
 
 void main()
 {
-    StructArray::StructArray_Main();
+//    StructArray::StructArray_Main();
 //    ConsoleDraw::ConsoleDraw_Main();
+//    AssocArray::AssocArray_Main();
 
     system("pause");
 }
