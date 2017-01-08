21939bedfe717bc434fc68ed978a2a6dfe2582bd
Alex J <alexander.aj.ranger@gmail.com>
2017-01-08 20:07:31 +0500

    .

--- StudentCpp/Basics_Extra_1_StructEnum.h
+++ StudentCpp/Basics_Extra_1_StructEnum.h
@@ -3,36 +3,4 @@
 
 #pragma once
 
-#include <iostream>
-using namespace std;
 
-namespace StructEnum
-{
-    enum MoneyType
-    {
-        Dollar,
-        Euro,
-        Ruble
-    };
-
-    struct MyStruct
-    {
-        int Num;
-        MoneyType Money;
-    };
-
-    int main()
-    {
-        // Перечисления штука несложная
-        MyStruct s;
-        s.Num = 666;
-        s.Money = Dollar;
-
-        if (s.Money == Dollar)
-            cout << "$";
-        if (s.Money == Euro)
-            cout << "Yohoo";
-        if (s.Money == Ruble)
-            cout << "Loser";
-    }
-}
