21691ea5a02314d1df54d9d1da730e6c146acf95
Alex J <alexander.aj.ranger@gmail.com>
2017-02-02 20:58:43 +0500

    КРУГИ АДА!

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,62 +1,6 @@
 ﻿#include <iostream>
 
-// Подключаем все подпрограммы
-#include "Basics_1_FirstProgram.h"
-#include "Basics_2.1_Variables.h"
-#include "Basics_2.2_GlobalVariables.h"
-#include "Basics_2.3_LocalVariables.h"
-#include "Basics_2.4_LocalGlobalVariables.h"
-#include "Basics_3_Cycles.h"
-#include "Basics_4_Functions.h"
-#include "Basics_5_Pointers.h"
-#include "Basics_6.1_References.h"
-#include "Basics_6.2_ReferencesVsPointers.h"
-#include "Basics_7.1_StaticArray.h"
-#include "Basics_7.2_TwoDimArray.h"
-#include "Basics_7.3_MultDimArray.h"
-#include "Basics_7.4_DynamicArray.h"
-#include "Basics_7.5_TwoDimDynamicArray.h"
-#include "Basics_8_FileIO.h"
-#include "Basics_9_Structures.h"
-#include "Basics_10_Unions.h"
-#include "Basics_11_Enum.h"
-#include "Basics_Extra_1_StructEnum.h"
-#include "Basics_Extra_2_StructArray.h"
-#include "Basics_Extra_3_StructList.h"
-#include "Basics_Extra_4_PassParams.h"
-
-#include "OOP_1.1_ClassBasics.h"
-#include "OOP_1.2_ClassLifeCycle.h"
-#include "OOP_1.3_ClassCopyConstructor.h"
-#include "OOP_2_OperatorOverloading.h"
-#include "OOP_3_Inheritance.h"
-#include "OOP_4.1_VirtualFunctions.h"
-#include "OOP_4.2_AbstactClass.h"
-#include "OOP_5_Polymorph.h"
-#include "OOP_Extra_AssocArray.h"
-
-#include "Templates_1_Templates.h"
-#include "Templates_2_TemplateList.h"
-#include "Templates_Extra_AssocArrayTemplate.h"
-
-#include "Zzz_AdvancedStreamIO.h"
-#include "Zzz_ConsoleDraw.h"
-#include "Zzz_ConsoleLanguage.h"
-
-using namespace std;
-
-int main()
-{
-//    StructArray::main();
-//    ConsoleDraw::main();
-//    AssocArray::main();
-//    Polymorph::main();
-//    ClassArray::main();
-//    ConsoleLanguage::main();
-    Unions::main();
-//    FileInputOutput::main();
-
-//    system("pause");
-
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ source/cmake-build-debug/CMakeFiles/source.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for source.
+# This may be replaced when dependencies are built.
