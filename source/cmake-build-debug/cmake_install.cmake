a7ab73a6b70363ad16c18dad44f7eb1c33e5cc48
Alex J <alexander.aj.ranger@gmail.com>
2017-02-18 15:27:34 +0500

    Добавлен пример для списка структур

--- source/cmake-build-debug/cmake_install.cmake
+++ source/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Programmes/StudentCpp/source
+# Install script for directory: D:/dev/StudentCpp/source
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Programmes/StudentCpp/source/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/dev/StudentCpp/source/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
