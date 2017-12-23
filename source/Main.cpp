b9d97ab859b58ec526fcfb724fc83696d9ac1486
Alex J <alexander.aj.ranger@gmail.com>
2017-12-23 22:56:17 +0500

    .

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,23 +1,25 @@
 // Подключаем все подпрограммы
-#include "Basics_1_FirstProgram.h"
-#include "Basics_2.1_Variables.h"
-#include "Basics_2.2_GlobalVariables.h"
-#include "Basics_2.3_LocalVariables.h"
-#include "Basics_2.4_LocalGlobalVariables.h"
-#include "Basics_3_Cycles.h"
-#include "Basics_4_Functions.h"
-#include "Basics_5_Pointers.h"
-#include "Basics_6.1_References.h"
-#include "Basics_6.2_ReferencesVsPointers.h"
-#include "Basics_7.1_StaticArray.h"
+#include "Basics_01_FirstProgram.h"
+#include "Basics_02.1_Variables.h"
+#include "Basics_02.2_GlobalVariables.h"
+#include "Basics_02.3_LocalVariables.h"
+#include "Basics_02.4_LocalGlobalVariables.h"
+#include "Basics_03_Cycles.h"
+#include "Basics_04_Functions.h"
+#include "Basics_05_Pointers.h"
+#include "Basics_06.1_References.h"
+#include "Basics_06.2_ReferencesVsPointers.h"
+#include "Basics_07.1_StaticArray.h"
 #include "Basics_7.2_DynamicArray.h"
 #include "Basics_7.3_Strings.h"
 #include "Basics_7.4_TwoDimArray.h"
 #include "Basics_7.5_MultDimArray.h"
 #include "Basics_7.6_DynamicArray.h"
 #include "Basics_7.7_TwoDimDynamicArray.h"
-#include "Basics_8_FileIO.h"
-#include "Basics_9_Structures.h"
+#include "Basics_08.1_Stream.h"
+#include "Basics_08.2_FoolProtection.h"
+#include "Basics_08.3_FileIO.h"
+#include "Basics_09_Structures.h"
 #include "Basics_10_Unions.h"
 #include "Basics_11_Enum.h"
 #include "Basics_Extra_01_StructEnum.h"
@@ -29,6 +31,7 @@
 #include "OOP_01.2_Encapsulation.h"
 #include "OOP_01.3_ClassLifeCycle.h"
 #include "OOP_01.4_CopyConstructor.h"
+#include "OOP_01.5_ArrayOfObjects.h"
 #include "OOP_02_OperatorOverloading.h"
 #include "OOP_03_Inheritance.h"
 #include "OOP_04.1_VirtualFunctions.h"
@@ -45,15 +48,15 @@
 #include "Zzz_ConsoleLanguage.h"
 
 #include <iostream>
+
+
 using namespace std;
 
 int main()
 {
     setlocale(0, "Rus");
 
-
-
-    Strings::main();
+//    Strings::main();
 //    Cycles::main();
 //    StructArray::main();
 //    ConsoleDraw::main();
@@ -66,12 +69,12 @@ int main()
 //    Variables::main();
 //    GlobalVariables::main();
 //    LocalVariables::main();
-
-//	Classes::main();
+//
+//	  Classes::main();
 //    Encapsulation::main();
-    // ClassLifeCycle::main();
+//    ClassLifeCycle::main();
 //    Functions::main();
-    FoolProtection::main();
+//    FoolProtection::main();
 
     return 0;
 }
