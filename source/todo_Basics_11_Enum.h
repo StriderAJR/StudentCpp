8e08a5bcfccf9624220955f3776323c645585a41
Alex J <alexander.aj.ranger@gmail.com>
2018-05-12 18:28:32 +0500

    source: marked undone files

---
+++ source/todo_Basics_11_Enum.h
@@ -0,0 +1,36 @@
+// Рассматриваемые темы:
+// Перечисления
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace Enum
+{
+    // Перечисление - пользовательский тип данных,
+    // который состоит из перечисленного набора констант
+    // Переменная такого типа данных может принимать только
+    // строго заданный набор значений
+    enum Currency
+    {
+        Dollar, Euro, Ruble, Pound
+    }; // <== Опять же не забываем про ';'!!!
+
+    struct BankAccount
+    {
+        int sum;
+        Currency currency;
+    };
+
+    void main()
+    {
+        BankAccount ivan;
+        ivan.sum = 1200;
+        ivan.currency = Ruble;
+
+        BankAccount victor;
+        victor.sum = 5000;
+        victor.currency = Dollar;
+    }
+}
